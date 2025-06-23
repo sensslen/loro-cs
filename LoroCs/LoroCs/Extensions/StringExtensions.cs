0ecf3eddf7c8fbe2a76f68d6fa9a23bfe4521b1f
Simon Ensslen <sensslen@users.noreply.github.com>
2025-06-23 09:34:09 +0200

    add some minimal infrastructure and correct readme (#16)

---
+++ LoroCs/LoroCs/Extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using uniffi.loro;
+
+namespace LoroCs.Extensions;
+
+public static class StringExtensions
+{
+    public static LoroValueLike ToLoroValueLike(this LoroValue value) => new ValueLike(value);
+
+    private sealed class ValueLike(LoroValue value) : LoroValueLike
+    {
+        public LoroValue AsLoroValue() => value;
+    }
+}
